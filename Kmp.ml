92a7a942626a9838e6224853817dbf1b29806082
dazdndcunfusd <dazdndcunfusd@gmai.com>
2019-03-28 11:37:11 -0400

    daily work, implemented table preprocessing
    implemented a *near* KMP algorithm, with table preprocessing. Might need fixing if considered iterative

--- Kmp.ml
+++ Kmp.ml
@@ -7,35 +7,37 @@ let assertInterrupted = function
   | Interrupted j -> j
 
 let print_result result =
-        match result with (*TODO : add extra dialogue for success and failure *)
-        | Found x -> print_string "found result at character ";print_int x ; print_newline ()
-        | Interrupted x -> print_string "Error at line ";print_int x ; print_newline ()
+        match result with 
+        | Found x -> print_string "found result at character ";print_int (x + 1) ; print_newline ()
+        | Interrupted x -> print_string "Error at character ";print_int (x + 1) ; print_newline ()
 
 (* Brute force Implementation -- NOT KMP *)
-let rec search pattern lpattern text ltext j k =
+let rec search pattern lpattern text ltext table j k =
         if j = lpattern then
                 Found (k - j)
         else if k = ltext then
                 Interrupted j
         else if pattern.[j] = text.[k] then
-                search pattern lpattern text ltext (j + 1) (k + 1)
+                search pattern lpattern text ltext table (j + 1) (k + 1)
         else if j = 0 then
-                search pattern lpattern text ltext 0 (k+1)
+                search pattern lpattern text ltext table 0 (k+1)
         else
-                let l = assertInterrupted (search pattern lpattern pattern j 0 1) in
-                assert (l<j);
-                search pattern lpattern text ltext l k
+                search pattern lpattern text ltext table table.(j) k
 
         (* lots of backtracking. O(m*(n-1)) AKA terrible *)
         (* l, j, and k are all iterators, keeping track of the last known position. *)
 
+(*pre-processing the table *)
+let init pattern m = 
+        let table = Array.make m 0 in
+        for j = 2 to m-1 do
+                table.(j) <- assertInterrupted (
+                search pattern m pattern j table table.(j - 1) (j -1)
+                )
+        done;
+        table
 
-(* checks if pattern length is valid. *)
-let prelim_check pattern pattern_len text text_len = 
-        if (pattern_len > text_len) || (pattern_len <1) || (text_len < 1) then
-                Interrupted 0
-        else
-                search pattern pattern_len text text_len 0 0
+     
 
 let check_if_file s1 s2 =
         () (*TODO: add functionality for text files as well. returns 1 if file, 0 if string. *)
@@ -49,5 +51,6 @@ let () =
         let pattern_len = String.length pattern in
         let text_len = String.length text in
         (* currently only accepts pure string arguments, not text files *)
-        let result = prelim_check pattern pattern_len text text_len in
+        let table = init pattern pattern_len in
+        let result = search pattern pattern_len text text_len table 0 0 in
         print_result result
